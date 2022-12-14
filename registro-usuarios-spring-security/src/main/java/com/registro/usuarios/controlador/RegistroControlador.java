15d20d8d1c6b668491d719bb2f5a73b8ddd7c146
Christofer66 <74540558@certus.edu.pe>
2022-12-14 09:15:57 -0500

    Ultimos cambios

--- registro-usuarios-spring-security/src/main/java/com/registro/usuarios/controlador/RegistroControlador.java
+++ registro-usuarios-spring-security/src/main/java/com/registro/usuarios/controlador/RegistroControlador.java
@@ -1,14 +1,11 @@
 package com.registro.usuarios.controlador;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.registro.usuarios.model.Consulta;
 import com.registro.usuarios.servicio.UsuarioServicio;
 
 @Controller
--- registro-usuarios-spring-security/src/main/java/com/registro/usuarios/model/Consulta.java
+++
@@ -1,5 +0,0 @@
-package com.registro.usuarios.model;
-
-public class Consulta {
-
-}
